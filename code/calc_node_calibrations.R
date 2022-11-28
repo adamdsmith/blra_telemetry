c89e03151e25208f1d0ea3cadf4ceb6493c05e84
Adam Smith <avianmigration@gmail.com>
2022-11-28 16:07:39 -0500

    Improved node calibration figures

--- code/calc_node_calibrations.R
+++ code/calc_node_calibrations.R
@@ -54,15 +54,44 @@ if (!update_node_cal) {
                                by = c("NodeId", "RadioId")) %>%
     mutate(NodeId = factor(NodeId),
            TagRSSI_cent = TagRSSI - mean(naive_node_corrections$TagRSSI_mean))
+  
+  # Plot centered received RSS by Node Position
+  ggplot(node_cal_beeps, aes(NodePos, TagRSSI_cent, color = NodeId)) + 
+    geom_point(alpha = 0.5, position = "jitter") +
+    labs(x = "Node position", y = "Received RSS (centered)") +
+    scale_x_continuous(breaks = 1:12, minor_breaks = NULL) +
+    theme_bw() +
+    theme(legend.position = "none")
+  ggsave("output/figures/node_RSS_by_calibration_position.png", width = 4.5, height = 4.5)
+  
+  
   message("Fitting GAM to control for node position around test tag during calibration.")
   node_cal_gam <- gam(TagRSSI_cent ~ NodeId + s(NodePos, bs = "cc") - 1,
                       method = "ML", data = node_cal_beeps)
+  new_dat <- data.frame(NodePos = 1:12,
+                        NodeId = "32645F")
+  preds <- as.data.frame(predict(node_cal_gam, newdata = new_dat, se.fit = TRUE))
+  new_dat <- cbind(new_dat, preds) %>%
+    mutate(fit = fit - 0.9052899) # Center corrections
+  ggplot(new_dat, aes(NodePos)) + 
+    geom_ribbon(aes(ymin = fit - se.fit, ymax = fit + se.fit), alpha = 0.5) + 
+    geom_line(aes(y = fit)) +
+    labs(x = "Node position", y = "RSS correction") +
+    scale_x_continuous(breaks = 1:12, minor_breaks = NULL) +
+    theme_bw()
+  ggsave("output/figures/node_position_effects_calibration.png", width = 6.5, height = 4.5)
   
   # Retrieve Node-level deviations (fixed effect estimates)
   node_coefs <- grep("NodeId", names(coef(node_cal_gam)))
   node_cal_gam.fe <- coef(node_cal_gam)[node_coefs]
   node_corrections <- tibble(NodeId = levels(node_cal_beeps$NodeId),
                              node_rssi_gam_adj = unname(node_cal_gam.fe))
+  mean(node_corrections$node_rssi_gam_adj); sd(node_corrections$node_rssi_gam_adj)
+  ggplot(node_corrections, aes(node_rssi_gam_adj)) +
+    geom_histogram(binwidth = 1, color = "black", fill = "gray50") +
+    labs(x = "RSS correction", y = NULL) + theme_bw()
+  ggsave("output/figures/node-specific_correction_histogram.png", width = 4.5, height = 4.5)
+  
   message("Saving node calibration corrections to '", node_correction_path, ".'")
   saveRDS(node_corrections, file = node_correction_path)
   with(left_join(node_corrections, naive_node_corrections), plot(mean_correction, node_rssi_gam_adj,
